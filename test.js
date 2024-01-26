bf0c25f419ad227ffe521cb1601aacd507f53ddb
TrungThang02 <trantrungthang01699516993@gmail.com>
2024-01-26 21:04:16 +0700

    up

--- test.js
+++ test.js
@@ -1,1 +1,31 @@
+const mqtt = require('mqtt');
 
+const brokerUrl = 'mqtt://mqtt-ttt.onrender.com';
+
+const client = mqtt.connect(brokerUrl);
+
+client.on('connect', () => {
+  console.log('Connected to MQTT server');
+
+  // Subscribe to a topic
+  client.subscribe('your/topic', (err) => {
+    if (err) {
+      console.error('Error subscribing to topic:', err);
+    } else {
+      console.log('Subscribed to topic: your/topic');
+    }
+  });
+
+  // Publish a message
+  client.publish('your/topic', 'Hello, MQTT!', (err) => {
+    if (err) {
+      console.error('Error publishing message:', err);
+    } else {
+      console.log('Message published to topic: your/topic');
+    }
+  });
+});
+
+client.on('message', (topic, message) => {
+  console.log(`Received message on topic ${topic}: ${message.toString()}`);
+});
